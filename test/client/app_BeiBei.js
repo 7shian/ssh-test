f7da7bd473787693b6d5d946d76a23141e542446
kc71486 <linda20030102@gmail.com>
2022-05-12 02:32:24 +0800

    modify method name to match ser.js

--- test/client/app_BeiBei.js
+++ test/client/app_BeiBei.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   })
   // switch to 分帳 page
   $('#__Page__menu__page2').click((event) => { 
-    $.get('./getHistoryDay', {}, (data) => {
+    $.get('./getHistoryDate', {}, (data) => {
       /*
         得到array(data)
           然後那個區塊的文字就是array[index].time array[index].item array[index].money
@@ -33,12 +33,18 @@ $(document).ready(function() {
     /*
       如何選到 #__Page6__bill__container 下的 #delete_id(index) 是不是被選擇
       date要怎麼拿到 => 在 #__Page6__bill__container 下的 #delete_id(index) 
-      post => splitMoneyDay
+      post => splitMoneyDate
       回傳標題 date item money    => $(#__Page18__bill_word).text(...)
       回傳分帳 username 需要付的錢 => #__Page18__personcheck__container 下的 #delete_id(index)
               username 需要付的錢
     */
-    // $.post('./splitMoneyDay', {date})
+    // $.post('./splitMoneyDate', {date})
+    /*
+    splitMoney: 
+    輸入 hid陣列 (hid可從getHistoryUnchecked獲得)
+    回傳 所有成員的 (uid, nickname, 欠錢(>0為欠錢))
+    */
+    // $.post('./splitMoney', {date})
   })
   // 確認分帳
   $('#__Page18__word__containerid2').click((event) => {
@@ -56,7 +62,7 @@ $(document).ready(function() {
   })
   // 選擇錢包之後 notification 需要展示出來
   $('#').click((event) => {
-    $.get('./showNotification', {}, (data) => { // this is not checked
+    $.get('./getNotification', {}, (data) => { // this is not checked
 
     })
   })
